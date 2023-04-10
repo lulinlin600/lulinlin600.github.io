5df037a2263ce2a5332d55d2ae963221561f9c60
lulinlin600 <44669916+lulinlin600@users.noreply.github.com>
2023-04-10 13:31:43 +0800

    Update kunyu77.js

--- test/lll/js/kunyu77.js
+++ test/lll/js/kunyu77.js
@@ -1,226 +1,37 @@
-import './dayjs.min.js'
-import './uri.min.js';
-import './crypto-js.js'
-import _ from './underscore-esm-min.js'
-
-let key = 'kunyu77';
-let url = 'http://api.kunyu77.com';
-let agent = 'Dalvik/2.1.0 (Linux; U; Android 6.0; ZTE BA520 Build/MRA58K)';
-let deviceId = '';
-let timeOffset = 0;
-
-var charStr = 'abacdefghjklmnopqrstuvwxyzABCDEFGHJKLMNOPQRSTUVWXYZ0123456789';
-
-function randIndex(min, max, i) {
-    let index = Math.floor(Math.random() * (max - min + 1) + min),
-        numStart = charStr.length - 10;
-    if (i == 0 && index >= numStart) {
-        index = randIndex(min, max, i);
-    }
-    return index;
-}
-
-function randomStr(len) {
-    let min = 0, max = charStr.length - 1, _str = '';
-    len = len || 15;
-    for (var i = 0, index; i < len; i++) {
-        index = randIndex(min, max, i);
-        _str += charStr[index];
-    }
-    return _str;
-}
-
-function request(reqUrl) {
-    let sj = dayjs().unix() - timeOffset;
-    let uri = new Uri(reqUrl);
-    uri.addQueryParam()
-    uri.addQueryParam('pcode', '010110004')
-    uri.addQueryParam('version', '2.0.4')
-    uri.addQueryParam('devid', deviceId)
-    uri.addQueryParam('package', 'com.sevenVideo.app.android')
-    uri.addQueryParam('sys', 'android')
-    uri.addQueryParam('sysver', '6.0')
-    uri.addQueryParam('brand', 'ZTE')
-    uri.addQueryParam('model', 'ZTE_BA520')
-    uri.addQueryParam('sj', sj);
-    let keys = [];
-    for (var i = 0; i < uri.queryPairs.length; i++) {
-        keys.push(uri.queryPairs[i][0]);
-    }
-    keys = _.sortBy(keys, function (name) { return name })
-    let tkSrc = uri.path();
-    for (let k of keys) {
-        tkSrc += uri.getQueryParamValue(k);
-    }
-    tkSrc += sj;
-    tkSrc += 'XSpeUFjJ';
-    console.log(tkSrc);
-    let tk = CryptoJS.MD5(tkSrc).toString().toLowerCase();
-    console.log(tk);
-    let header = {
-        'user-agent': agent,
-        't': sj,
-        'TK': tk,
-    };
-    let res = req(uri.toString(), {
-        headers: header
-    });
-
-    let serverTime = res.headers.Date;
-    let content = res.content;
-    let serverTimeS = dayjs(serverTime).unix();
-    timeOffset = dayjs().unix() - serverTimeS;
-    // console.log(content);
-    return content;
-}
-
-function init(ext) {
-    var device = local.get(key, 'device');
-    if (device.length == 0) {
-        device = randomStr(32).toLowerCase();
-        local.set(key, 'device', device);
-    }
-    deviceId = device;
-    console.log(dayjs().unix());
-}
-
-function home(filter) {
-    let data = JSON.parse(request(url + '/api.php/provide/filter')).data;
-    let classes = [];
-    for (const key in data) {
-        classes.push({
-            'type_id': key,
-            'type_name': data[key][0].cat
-        });
-    }
-    console.log(classes);
-    return JSON.stringify({
-        'class': classes
-    });
-}
-
-function homeVod(params) {
-    let data = JSON.parse(request(url + '/api.php/provide/homeBlock?type_id=0')).data;
-    let blocks = data.blocks;
-    let videos = [];
-    for (const block of blocks) {
-        let name = block.block_name;
-        if (name.indexOf('热播') >= 0)
-            continue;
-        let contents = block.contents;
-        for (const content of contents) {
-            videos.push({
-                'vod_id': content.id,
-                'vod_name': content.title,
-                'vod_pic': content.videoCover,
-                'vod_remarks': content.msg,
-            })
-        }
-    }
-    return JSON.stringify({
-        'list': videos
-    });
-}
-
-function category(tid, pg, filter, extend) {
-    let data = JSON.parse(request(url + '/api.php/provide/searchFilter?type_id=' + tid + '&pagenum=' + pg + '&pagesize=24')).data;
-    let videos = []
-    for (const vod of data.result) {
-        videos.push({
-            'vod_id': vod.id,
-            'vod_name': vod.title,
-            'vod_pic': vod.videoCover,
-            'vod_remarks': vod.msg,
-        });
-    }
-    return JSON.stringify({
-        'page': parseInt(data.page),
-        'pagecount': data.pagesize,
-        'limit': 24,
-        'total': data.total,
-        'list': videos,
-    });
-}
-
-function detail(id) {
-    let data = JSON.parse(request(url + '/api.php/provide/videoDetail?ids=' + id)).data;
-    let vod = {
-        'vod_id': data.id,
-        'vod_name': data.videoName,
-        'vod_pic': data.videoCover,
-        'type_name': data.subCategory,
-        'vod_year': data.year,
-        'vod_area': data.area,
-        'vod_remarks': data.msg,
-        'vod_actor': data.actor,
-        'vod_director': data.director,
-        'vod_content': data.brief.trim(),
-    }
-    let episodes = JSON.parse(request(url + '/api.php/provide/videoPlaylist?ids=' + id)).data.episodes;
-    let playlist = {};
-    for (const episode of episodes) {
-        let playurls = episode.playurls;
-        for (const playurl of playurls) {
-            let from = playurl.playfrom;
-            if (!playlist.hasOwnProperty(from)) {
-                playlist[from] = []
-            }
-            playlist[from].push(playurl.title.trim() + '$' + playurl.playurl);
-        }
-    }
-    vod.vod_play_from = _.keys(playlist).join('$$$');
-    let urls = _.values(playlist)
-    let vod_play_url = []
-    for (const urlist of urls) {
-        vod_play_url.push(urlist.join('#'))
-    }
-    vod.vod_play_url = vod_play_url.join('$$$')
-    return JSON.stringify({
-        'list': [vod]
-    });
-}
-
-function play(flag, id, flags) {
-    let data = JSON.parse(request(url + '/api.php/provide/parserUrl?url=' + id)).data;
-    let playHeader = data.playHeader;
-    let jxUrl = data.url;
-    let jxData = JSON.parse(request(jxUrl));
-    if (flags.indexOf(flag) >= 0) {
-        return JSON.stringify({
-            'parse': 1,
-            'jx': 1,
-            'url': id
-        });
-    } else {
-        return JSON.stringify({
-            'parse': 0,
-            'url': id
-        });
-    }
-}
-
-function search(wd, quick) {
-    let data = JSON.parse(request(url + '/api.php/provide/searchVideo?searchName=' + wd)).data;
-    let videos = [];
-    for (const vod of data) {
-        videos.push({
-            'vod_id': vod.id,
-            'vod_name': vod.videoName,
-            'vod_pic': vod.videoCover,
-            'vod_remarks': vod.msg,
-        })
-    }
-    return JSON.stringify({
-        'list': videos
-    });
-}
-
-__JS_SPIDER__ = {
-    init: init,
-    home: home,
-    homeVod: homeVod,
-    category: category,
-    detail: detail,
-    play: play,
-    search: search
+var rule = {
+    title:'酷云77',
+    host:'http://api.kunyu77.com',
+    homeUrl:'/api.php/provide/homeBlock?type_id=0',
+    searchUrl:'/api.php/provide/searchVideo?searchName=**',
+    searchable:2,
+    quickSearch:0,
+    filterable:1,
+    multi:1,
+    // 分类链接fypage参数支持1个()表达式
+    url:'/api.php/provide/searchFilter?type_id=fyclass&pagesize=24&pagenum=fypage',
+    detailUrl:'/api.php/provide/videoDetail?devid=453CA5D864457C7DB4D0EAA93DE96E66&package=com.sevenVideo.app.android&version=1.8.7&ids=fyid',
+    filter_url:'year={{fl.y}}&category={{fl.scat}}&area={{fl.a}}',
+    filter:{0: [{'key': 'y', 'name': '年代', 'value': [{'n': '全部', 'v': ''}, {'n': '2022', 'v': '2022'}, {'n': '2021', 'v': '2021'}, {'n': '2020', 'v': '2020'}, {'n': '2019', 'v': '2019'}, {'n': '2018', 'v': '2018'}, {'n': '2017', 'v': '2017'}, {'n': '2016', 'v': '2016'}, {'n': '2015', 'v': '2015'}, {'n': '2015之前', 'v': 'lt|2015'}]}, {'key': 'a', 'name': '地区', 'value': [{'n': '全部', 'v': ''}, {'n': '中国大陆', 'v': '中国大陆'}, {'n': '美国', 'v': '美国'}, {'n': '日本', 'v': '日本'}, {'n': '中国香港', 'v': '中国香港'}, {'n': '中国台湾', 'v': '中国台湾'}, {'n': '韩国', 'v': '韩国'}, {'n': '欧洲', 'v': '欧洲'}, {'n': '其他', 'v': '泰国'}]}, {'key': 'scat', 'name': '类型', 'value': [{'n': '全部', 'v': ''}, {'n': '剧情', 'v': '剧情'}, {'n': '动作', 'v': '动作'}, {'n': '科幻', 'v': '科幻'}, {'n': '喜剧', 'v': '喜剧'}, {'n': '战争', 'v': '战争'}, {'n': '爱情', 'v': '爱情'}, {'n': '恐怖', 'v': '恐怖'}, {'n': '伦理', 'v': '伦理'}]}], 1: [{'key': 'y', 'name': '年代', 'value': [{'n': '全部', 'v': ''}, {'n': '2022', 'v': '2022'}, {'n': '2021', 'v': '2021'}, {'n': '2020', 'v': '2020'}, {'n': '2019', 'v': '2019'}, {'n': '2018', 'v': '2018'}, {'n': '2017', 'v': '2017'}, {'n': '2016', 'v': '2016'}, {'n': '2015', 'v': '2015'}, {'n': '2015之前', 'v': 'lt|2015'}]}, {'key': 'a', 'name': '地区', 'value': [{'n': '全部', 'v': ''}, {'n': '中国大陆', 'v': '中国大陆'}, {'n': '美国', 'v': '美国'}, {'n': '日本', 'v': '日本'}, {'n': '中国香港', 'v': '中国香港'}, {'n': '中国台湾', 'v': '中国台湾'}, {'n': '韩国', 'v': '韩国'}, {'n': '欧洲', 'v': '欧洲'}, {'n': '其他', 'v': '泰国'}]}, {'key': 'scat', 'name': '类型', 'value': [{'n': '全部', 'v': ''}, {'n': '剧情', 'v': '剧情'}, {'n': '动作', 'v': '动作'}, {'n': '科幻', 'v': '科幻'}, {'n': '喜剧', 'v': '喜剧'}, {'n': '战争', 'v': '战争'}, {'n': '爱情', 'v': '爱情'}, {'n': '恐怖', 'v': '恐怖'}, {'n': '伦理', 'v': '伦理'}]}], 2: [{'key': 'y', 'name': '年代', 'value': [{'n': '全部', 'v': ''}, {'n': '2022', 'v': '2022'}, {'n': '2021', 'v': '2021'}, {'n': '2020', 'v': '2020'}, {'n': '2019', 'v': '2019'}, {'n': '2018', 'v': '2018'}, {'n': '2017', 'v': '2017'}, {'n': '2016', 'v': '2016'}, {'n': '2015', 'v': '2015'}, {'n': '2015之前', 'v': 'lt|2015'}]}, {'key': 'a', 'name': '地区', 'value': [{'n': '全部', 'v': ''}, {'n': '中国大陆', 'v': '中国大陆'}, {'n': '美国', 'v': '美国'}, {'n': '日本', 'v': '日本'}, {'n': '中国香港', 'v': '中国香港'}, {'n': '中国台湾', 'v': '中国台湾'}, {'n': '韩国', 'v': '韩国'}, {'n': '欧洲', 'v': '欧洲'}, {'n': '其他', 'v': '泰国'}]}, {'key': 'scat', 'name': '类型', 'value': [{'n': '全部', 'v': ''}, {'n': '剧情', 'v': '剧情'}, {'n': '动作', 'v': '动作'}, {'n': '科幻', 'v': '科幻'}, {'n': '喜剧', 'v': '喜剧'}, {'n': '战争', 'v': '战争'}, {'n': '爱情', 'v': '爱情'}, {'n': '恐怖', 'v': '恐怖'}, {'n': '伦理', 'v': '伦理'}]}], 3: [{'key': 'y', 'name': '年代', 'value': [{'n': '全部', 'v': ''}, {'n': '2022', 'v': '2022'}, {'n': '2021', 'v': '2021'}, {'n': '2020', 'v': '2020'}, {'n': '2019', 'v': '2019'}, {'n': '2018', 'v': '2018'}, {'n': '2017', 'v': '2017'}, {'n': '2016', 'v': '2016'}, {'n': '2015', 'v': '2015'}, {'n': '2015之前', 'v': 'lt|2015'}]}, {'key': 'a', 'name': '地区', 'value': [{'n': '全部', 'v': ''}, {'n': '中国大陆', 'v': '中国大陆'}, {'n': '美国', 'v': '美国'}, {'n': '日本', 'v': '日本'}, {'n': '中国香港', 'v': '中国香港'}, {'n': '中国台湾', 'v': '中国台湾'}, {'n': '韩国', 'v': '韩国'}, {'n': '欧洲', 'v': '欧洲'}, {'n': '其他', 'v': '泰国'}]}, {'key': 'scat', 'name': '类型', 'value': [{'n': '全部', 'v': ''}, {'n': '剧情', 'v': '剧情'}, {'n': '动作', 'v': '动作'}, {'n': '科幻', 'v': '科幻'}, {'n': '喜剧', 'v': '喜剧'}, {'n': '战争', 'v': '战争'}, {'n': '爱情', 'v': '爱情'}, {'n': '恐怖', 'v': '恐怖'}, {'n': '伦理', 'v': '伦理'}]}], 4: [{'key': 'y', 'name': '年代', 'value': [{'n': '全部', 'v': ''}, {'n': '2022', 'v': '2022'}, {'n': '2021', 'v': '2021'}, {'n': '2020', 'v': '2020'}, {'n': '2019', 'v': '2019'}, {'n': '2018', 'v': '2018'}, {'n': '2017', 'v': '2017'}, {'n': '2016', 'v': '2016'}, {'n': '2015', 'v': '2015'}, {'n': '2015之前', 'v': 'lt|2015'}]}, {'key': 'a', 'name': '地区', 'value': [{'n': '全部', 'v': ''}, {'n': '中国大陆', 'v': '中国大陆'}, {'n': '美国', 'v': '美国'}, {'n': '日本', 'v': '日本'}, {'n': '中国香港', 'v': '中国香港'}, {'n': '中国台湾', 'v': '中国台湾'}, {'n': '韩国', 'v': '韩国'}, {'n': '欧洲', 'v': '欧洲'}, {'n': '其他', 'v': '泰国'}]}, {'key': 'scat', 'name': '类型', 'value': [{'n': '全部', 'v': ''}, {'n': '剧情', 'v': '剧情'}, {'n': '动作', 'v': '动作'}, {'n': '科幻', 'v': '科幻'}, {'n': '喜剧', 'v': '喜剧'}, {'n': '战争', 'v': '战争'}, {'n': '爱情', 'v': '爱情'}, {'n': '恐怖', 'v': '恐怖'}, {'n': '伦理', 'v': '伦理'}]}]},
+    headers:{
+		"User-Agent":"okhttp/3.12.0"
+	},
+    timeout:5000,
+    class_name:'全部&电影&电视剧&综艺&动漫',
+    class_url:'0&1&2&3&4',
+    limit:20,
+    play_parse:true,
+    play_json:0,
+    // 手动调用解析请求json的url,此lazy不方便
+    // lazy:'js:input={parse:1,url:input};',
+    lazy:'',
+    lazy:'js:function GetPlayUrl(playUrl){let realPlay={parse:0,url:playUrl};if(/mgtv|sohu/.test(playUrl)){realPlay.headers={"User-Agent":"Mozilla/5.0"}}else if(/bili/.test(playUrl)){realPlay.headers={"User-Agent":"Mozilla/5.0",Referer:"https://www.bilibili.com"}}else if(/ixigua/.test(playUrl)){realPlay.headers={"User-Agent":"Mozilla/5.0",Referer:"https://www.ixigua.com"}}return realPlay}if(/\\.m3u8|\\.mp4/.test(input)){input={parse:0,url:input}}else{try{let jxUrl="http://api.kunyu77.com/api.php/provide/parserUrl?url=";var t=Math.floor((new Date).getTime()/1e3).toString();let jxExt="&retryNum=0&pcode=010110002&version=2.1&devid=f9c9ce5bb5827a266829383718e6131a&package=com.sevenVideo.app.android&sys=android&sysver=12&brand=Xiaomi&model=Mi_10_Pro&sj="+t;let url=jxUrl+input+jxExt;let TK="/api.php/provide/parserUrl"+"Xiaomif9c9ce5bb5827a266829383718e6131aMi_10_Procom.sevenVideo.app.android010110002"+0+t+"android12"+encodeURIComponent(vipUrl)+"2.1"+t+"XSpeUFjJ";let html=request(url,{headers:{Referer:jxUrl,"User-Agent":"okhttp/3.12.0",TK:md5(TK)}});let urll=JSON.parse(html).data.url;let playhtml=request(urll);let playurl=JSON.parse(playhtml).url;input=GetPlayUrl(playurl)}catch(e){input={parse:1,jx:1,url:input}}}',
+    推荐:'json:data.blocks;contents;title;videoCover;msg;id',
+    double:true,
+    // 一级:'json:data.filterData.listData;title;img;subTitle;videoLink;summary',
+    一级:'',
+    一级:'json:data.result;title;videoCover;msg;id',
+    二级:'',
+    二级:'js:var d=[];VOD={vod_id:input};try{let html=request(input);print(html);html=JSON.parse(html);let node=html.data;VOD={vod_id:node["id"],vod_name:node["videoName"],vod_pic:node["videoCover"],type_name:node["subCategory"],vod_year:node["year"],vod_area:node["area"],vod_remarks:node["msg"],vod_actor:node["actor"],vod_director:node["director"],vod_content:node["brief"].strip()};let tid=input.split("ids=")[1];let listUrl="http://api.kunyu77.com/api.php/provide/videoPlaylist?devid=453CA5D864457C7DB4D0EAA93DE96E66&package=com.sevenVideo.app.android&version=1.8.7&ids="+tid;html=request(listUrl);html=JSON.parse(html);let episodes=html.data.episodes;let playMap={};if(typeof play_url==="undefined"){var play_url=""}play_url=play_url.replace("&play_url=","&type=json&play_url=");episodes.forEach(function(ep){let playurls=ep["playurls"];playurls.forEach(function(playurl){let source=playurl["playfrom"];if(!playMap.hasOwnProperty(source)){playMap[source]=[]}playMap[source].append(playurl["title"].strip()+"$"+play_url+urlencode(playurl["playurl"]))})});let playFrom=[];let playList=[];Object.keys(playMap).forEach(function(key){playFrom.append(key);playList.append(playMap[key].join("#"))});let vod_play_from=playFrom.join("$$$");let vod_play_url=playList.join("$$$");VOD["vod_play_from"]=vod_play_from;VOD["vod_play_url"]=vod_play_url}catch(e){log("获取二级详情页发生错误:"+e.message)}',
+    搜索:'',
+    搜索:'json:data;videoName;videoCover;msg;id',
 }
